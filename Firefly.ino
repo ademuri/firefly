20edb2d80df5cd3868a7c439f78e71f36ea8ad47
Adam Demuri <adam.demuri@gmail.com>
2016-04-26 21:55:38 -0600

    Add GPLv3

--- Firefly.ino
+++ Firefly.ino
@@ -1,3 +1,23 @@
+/*
+ *  Firefly - Arduino software that drives an LED. It uses a CC1101 to sync between nodes,
+ *  and a microphone to sync to music.
+ *
+ *  Copyright (C) 2016 Adam Demuri
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #include "Arduino.h"
 
 #include "cc1101/cc1101.h"
